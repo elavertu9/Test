74b7907e10d546097340f6814c0fba9882bedaa0
elavertu9 <elavertu9@gmail.com>
2022-07-20 16:06:50 -0500

    Try with python

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-def hello():
-    print("Hello")
+import time
 
 
-def bye():
-    print("Bye")
+def wait():
+    print("Waiting 10 seconds...")
+    time.sleep(10)
+
+
+def create_file_in_mount_dir():
+    print("Creating file in mount dir")
+    with open('/test-vol-dir/test.txt', 'w') as file:
+        file.write("Hello from inside the container!")
 
 
 if __name__ == "__main__":
-    hello()
+    wait()
+    create_file_in_mount_dir()
