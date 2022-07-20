aa068a73797178cd5fd8dc7a3d489168fa6a39ae
elavertu9 <elavertu9@gmail.com>
2022-07-20 15:35:23 -0500

    Update compose file and start script

--- docker/resources/start.sh
+++ docker/resources/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running start script inside the container..."
-echo "Hello from inside the container" > /tmp/test.txt
+echo "Hello from inside the container" > /test-vol-dir/test.txt
 tail -f /dev/null
