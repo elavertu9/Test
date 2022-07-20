846964406bcc9770d8b921fc843691249f1eaf36
elavertu9 <elavertu9@gmail.com>
2022-07-20 15:28:43 -0500

    Update start.sh

--- docker/resources/start.sh
+++ docker/resources/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running start script inside the container..."
-echo "Hello from inside the container" > /test-vol/test.txt
+echo "Hello from inside the container" > /tmp/test.txt
 tail -f /dev/null
