32b36c123da04f359d921bf292cb53ff70295336
elavertu9 <elavertu9@gmail.com>
2022-08-04 14:17:05 -0600

    Update start.sh

--- var-test/start.sh
+++ var-test/start.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 echo "Hello from start.sh..."
 
-echo "$TEST_SECRET_ENV"
+#echo "$TEST_SECRET_ENV"
 
-echo "$TESTER"
+echo ${Hello}
 
 HELLO_COMPARISON="Hello"
 
 #if [ "$TEST_SECRET_ENV" == "testsecret" ];
-if [[ "$HELLO" == "$HELLO_COMPARISON" ]]; then
+if [ ${HELLO} == ${HELLO_COMPARISON} ]; then
   echo "The secret is what you expect"
 else
   echo "The secret is NOT what you expect"
